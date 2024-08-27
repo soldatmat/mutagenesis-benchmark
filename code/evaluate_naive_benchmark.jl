5becd2f47398c411bbd3d3e585cf01e70be2cfa3
soldamatlab <matoussoldat@email.cz>
2024-08-27 10:08:42 +0200

    Refactor evaluation

--- code/evaluate_naive_benchmark.jl
+++ code/evaluate_naive_benchmark.jl
@@ -1,24 +1,7 @@
-using XLSX
-
-include("naive_benchmark.jl")
 include("utils.jl")
+include("evaluation.jl")
 
 Random.seed!(42)
-
-function prepare_data(dataset_name::String)
-    file_path = joinpath(@__DIR__, "..", "data", "preprocessed_data", dataset_name, dataset_name * ".csv")
-    df = CSV.read(file_path, DataFrame)
-    rename!(df, :fitness => :score)
-    df.gap = get_mutational_gaps(df)
-    return df
-end
-
-function prepare_data_combinatorial(dataset_name::String)
-    file_path = joinpath(@__DIR__, "..", "data", "combinatorial", dataset_name, "ground_truth.csv")
-    df = CSV.read(file_path, DataFrame)
-    df.gap = get_mutational_gaps(df)
-    return df
-end
 
 """
 # Arguments
@@ -31,20 +14,7 @@ function evaluate_method(df::DataFrame; n_mutants::Int)
     df_train = CONSTRUCT_TRAIN_SET(df)
     println("Size of training set = $(size(df_train)[1])")
     mutants = SELECT_MUTANTS(df_train; n_mutants=n_mutants)
-
-    # ___ Prepare Evaluation ___
-    mutant_strings = map(i -> String(mutants[i]), eachindex(mutants))
-    default_score = denormalize_score(0.0, df) # corresponds to minimum(df.score)
-    scores = map(mutant -> filter(row -> row.sequence == mutant, df).score, mutant_strings)
-    n_replaced_scores = mapreduce(s -> length(s) == 0, +, scores) # How many score will be replaced with default_score?
-    scores = map(s -> length(s) == 0 ? default_score : mean(s), scores)
-
-    # ___ Evaluate ___
-    fitness = median(scores)
-    fitness_norm = normalize_score(median(scores), df)
-    diversity = median(pairwise(hamming, mutants))
-    novelty = median(map(col -> minimum(col), eachcol(pairwise(hamming, df_train.sequence, mutants))))
-    return (fitness, fitness_norm, diversity, novelty, n_replaced_scores, length(mutants))
+    evaluate_mutants(mutants, df, df_train)
 end
 
 function evaluation_iteration(dataset_name::String; n_mutants::Int=128)
---
+++ code/evaluation.jl
@@ -0,0 +1,33 @@
+include("naive_benchmark.jl")
+
+function prepare_data(dataset_name::String)
+    file_path = joinpath(@__DIR__, "..", "data", "preprocessed_data", dataset_name, dataset_name * ".csv")
+    df = CSV.read(file_path, DataFrame)
+    rename!(df, :fitness => :score)
+    df.gap = get_mutational_gaps(df)
+    return df
+end
+
+function prepare_data_combinatorial(dataset_name::String)
+    file_path = joinpath(@__DIR__, "..", "data", "combinatorial", dataset_name, "ground_truth.csv")
+    df = CSV.read(file_path, DataFrame)
+    df.gap = get_mutational_gaps(df)
+    return df
+end
+
+function evaluate_mutants(mutants::Vector{Vector{Char}}, df::DataFrame, df_train::DataFrame)
+    # ___ Prepare Evaluation ___
+    mutant_strings = map(i -> String(mutants[i]), eachindex(mutants))
+    default_score = denormalize_score(0.0, df) # corresponds to minimum(df.score)
+    scores = map(mutant -> filter(row -> row.sequence == mutant, df).score, mutant_strings)
+    n_replaced_scores = mapreduce(s -> length(s) == 0, +, scores) # How many score will be replaced with default_score?
+    scores = map(s -> length(s) == 0 ? default_score : mean(s), scores)
+
+    # ___ Evaluate ___
+    fitness = median(scores)
+    fitness_norm = normalize_score(median(scores), df)
+    diversity = median(pairwise(hamming, mutants))
+    novelty = median(map(col -> minimum(col), eachcol(pairwise(hamming, df_train.sequence, mutants))))
+
+    return (fitness, fitness_norm, diversity, novelty, n_replaced_scores, length(mutants))
+end
