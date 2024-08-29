4d41ae62022865e69844a31ec905a5014585633e
Matouš Soldát <matoussoldat@email.cz>
2024-08-29 16:17:40 +0200

    Run FLIP ridge on short sequences

--- code/flip_ridge.py
+++ code/flip_ridge.py
@@ -51,7 +51,7 @@ def run_ridge(train, test):
 
 if __name__ == '__main__':
     datasets = ["GB1", "PhoQ", "TrpB"]
-    splits = ["non-zero_medium_gap0", "non-zero_hard_gap0"]
+    splits = ["non-zero_medium_gap0_short", "non-zero_hard_gap0_short"]
 
     for dataset in datasets:
         for split in splits:
